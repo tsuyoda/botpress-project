703b7b10fb1dbd996e221d145c992ea9ba59e737
Lucas Toyoda <lucastoyoda00@gmail.com>
2023-06-04 02:25:55 -0300

    feat: add email

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(bot.tsuyoda.cloud www.bot.tsuyoda.cloud)
 rsa_key_size=4096
 data_path="./certbot"
-email="" # Adding a valid address is strongly recommended
+email="lucastoyoda00@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
